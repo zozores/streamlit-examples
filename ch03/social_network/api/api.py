4a2f45e5ebc885159d0f742ace7dd0cc8af9b71d
Thiago Ozores <tozores@gmail.com>
2023-01-12 09:42:58 -0300

    updating ch03

--- ch03/social_network/api/api.py
+++ ch03/social_network/api/api.py
@@ -1,24 +1,20 @@
 from models import Post
 import datetime
 
+posts = []
+
 
 class API:
     def __init__(self, config=None):
         self.config = config
 
     def add_post(self, post: Post):
-        return True
+        posts_before = len(posts)
+        posts.append(post)
+        if posts_before < len(posts):
+            return True
+        else:
+            return False
 
     def get_posts(self, start_date: datetime.datetime, end_date: datetime.datetime):
-        return [
-            Post(
-                "Adam",
-                "Python is a snake",
-                datetime.datetime(year=2021, month=5, day=1),
-            ),
-            Post(
-                "Sara",
-                "Python is a programming language",
-                datetime.datetime(year=2021, month=5, day=3),
-            ),
-        ]
+        return posts
--- ch03/social_network/main.py
+++ ch03/social_network/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from views import FeedView, AddPostView
 from services import get_feed, add_post
 
+
 AddPostView(add_post)
 st.write("____")
 FeedView(get_feed)
